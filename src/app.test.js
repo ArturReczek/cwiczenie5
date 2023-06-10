de5e9a653a3e79df1f29a09c44cc434863e41f55
ArturReczek <131576667+ArturReczek@users.noreply.github.com>
2023-06-10 13:47:18 +0200

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,12 @@
-const app = require("./app");
-const supertest = require("Supertest")
+const app = require("./app")
+const supertest = require("supertest")
 const request = supertest(app)
 
-describe("/test endpoint", () => {
-	it("powinna zwrócić się odpowiedź", async () => {
-	const response = await reguest.get("./test)
-	expect(response.status).toBe(200);
-	expect(response.test).toBe("Hello World");
-})
+describe("/test endopint", () => {
+	it("Aplikacja powinna zwrócić odpowiedź", async() => {
+	const response = await request.get("/test")
+	expect(response.status).toBe(200)
+	expect(response.text).toBe("Hello World")
 
+})
 })
